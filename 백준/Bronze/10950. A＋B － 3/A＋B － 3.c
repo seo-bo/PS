c553df0abb0f12e692831f26394b7948125962b2
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 15:54:55 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 0;
+
+	scanf("%d", &num1);
+	for (int i = 0; i < num1; i++)
+	{
+		int num2 = 0;
+		int num3 = 0;
+		scanf("%d %d", &num2, &num3);
+		printf("%d\n", num2 + num3);
+	}
+}
