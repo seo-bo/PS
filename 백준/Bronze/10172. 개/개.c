75213d0ad2bd37ca899f0d4c95c07d3af9fef898
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 15:46:08 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    | \n");
+	printf("||_/=\\\\__|");
+	
+}
