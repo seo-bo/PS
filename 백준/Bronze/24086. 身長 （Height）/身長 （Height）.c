2ececc0ef8fbf66ef618a4f9ce70836d62fccbb3
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 17:22:53 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int num1 = 0;
+    int num2 = 0;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    printf("%d", num2 - num1);
+}
+
