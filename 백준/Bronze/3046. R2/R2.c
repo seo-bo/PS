512b8ede5d48380277966d33979901bf852b2b51
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 15:07:47 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf("%d %d", &num1, &num2);
+	int num3 = (2 * num2) - num1;
+	printf("%d", num3);
+}
+
+
