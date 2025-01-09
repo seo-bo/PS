e6651d567817afa30df1be9da1e5d95e234ce05f
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 17:22:45 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 1300 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int num1 = 0;
+    scanf("%d", &num1);
+    int result = pow(num1, 3);
+    printf("%d", result);
+}
+
