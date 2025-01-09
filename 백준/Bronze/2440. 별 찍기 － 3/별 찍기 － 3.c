f586830274917c6644e548a89c0c3aad58008c90
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 14:44:53 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int num = 0;
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = num - i; j > 0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
