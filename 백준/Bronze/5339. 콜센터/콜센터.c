93a23f8c5cd4a62d58ab4c25d730e9e75dbc877b
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 15:21:34 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\\n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	
+}
