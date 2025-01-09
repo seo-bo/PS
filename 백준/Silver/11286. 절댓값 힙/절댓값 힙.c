ef68e85ae53ee06b60a78f82516108f371a9fc69
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 16:03:42 +0900

    [Silver I] Title: 절댓값 힙, Time: 20 ms, Memory: 1508 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct Heap
+{
+	int heap[100001];
+	int size;
+}Heap;
+
+Heap* create()
+{
+	return (Heap*)malloc(sizeof(Heap));
+}
+
+void init(Heap* h)
+{
+	h->size = 0;
+}
+
+void insert_min(Heap* h, int num)
+{
+	int i = ++(h->size);
+	while ((i != 1) && ((abs(num) < abs(h->heap[i / 2])) || (abs(num) == abs(h->heap[i / 2]) && num < h->heap[i / 2])))
+	{
+		h->heap[i] = h->heap[i / 2];
+		i /= 2;
+	}
+	h->heap[i] = num;
+}
+
+int delete_min(Heap* h)
+{
+	if (h->size == 0)
+	{
+		return 0;
+	}
+	int parent = 1, child = 2;
+	int num = h->heap[1];
+	int temp = h->heap[(h->size)--];
+	while (child <= h->size)
+	{
+		if ((child < h->size) && ((abs(h->heap[child]) > abs(h->heap[child + 1])) || (abs(h->heap[child]) == abs(h->heap[child + 1]) && h->heap[child] > h->heap[child + 1])))
+		{
+			child++;
+		}
+		if (!(abs(temp) > abs(h->heap[child]) || (abs(temp) == abs(h->heap[child]) && temp > h->heap[child])))
+		{
+			break;
+		}
+		h->heap[parent] = h->heap[child];
+		parent = child;
+		child *= 2;
+	}
+	h->heap[parent] = temp;
+	return num;
+}
+
+int main(void)
+{
+	Heap* h = create();
+	init(h);
+	int num = 0;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++)
+	{
+		int get = 0;
+		scanf("%d", &get);
+		if (get == 0)
+		{
+			printf("%d\n", delete_min(h));
+		}
+		else
+		{
+			insert_min(h, get);
+		}
+	}
+}
+
