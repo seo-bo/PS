a8c175d32f19e837e024b10ff8b3ee2455590dd2
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 15:21:19 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+}
