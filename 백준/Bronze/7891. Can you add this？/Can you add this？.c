27d91b7510e8994f51a7dcc47422880e4a3a4bd1
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 15:33:44 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int num = 0;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++)
+	{
+		int num1 = 0;
+		int num2 = 0;
+		scanf("%d %d", &num1, &num2);
+		printf("%d\n", num1 + num2);
+	}
+}
