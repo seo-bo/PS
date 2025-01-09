d167e08a6f3ba0369a758f3bb80d6d4221f0d40e
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 17:16:24 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int num1, num2, num3, num4, num5, num6= 0;
+    scanf("%d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6);
+    printf("%d",5*(num1+num2+num3+num4+num5+num6));
+}
