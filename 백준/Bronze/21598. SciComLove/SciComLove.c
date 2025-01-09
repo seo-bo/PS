4491e1810c66688e860541ec76e64a8f22b6317e
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-09 17:16:39 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int num1 = 0;
+    scanf("%d", &num1);
+    for (int i = 0; i < num1; i++)
+    {
+        printf("SciComLove\n");
+    }
+}
