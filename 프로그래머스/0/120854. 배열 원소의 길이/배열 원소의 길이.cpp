8b02ddf4912a3d68e528d58ebc9c9d74bc58d12f
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-27 08:19:16 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    for(auto & i : strlist)
+    {
+        answer.push_back(i.size());
+    }
+    return answer;
+}
