6b99b2283e8651bd47ffe08c4833f344c319e6d1
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-27 08:39:15 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if(number % n == 0 && number % m == 0)
+    {
+        return 1;
+    }
+    return answer;
+}
