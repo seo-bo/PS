f3cd2371b366d60cca884248b22f28df3895b0b7
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-27 10:25:23 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    if(a == b && b == c)
+    {
+        return (a+b+c) * (a*a + b*b + c*c) * (a * a * a + b * b * b + c * c * c);
+    }
+    else if (a != b && b != c && a != c)
+    {
+        return a + b + c;
+    }
+    else
+    {
+        return (a+b+c) * (a*a + b*b + c*c); 
+    }
+    return answer;
+}
