82a1c452c585ba3749dc5e46cc2aeb3e5ea18465
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-27 09:53:08 +0900

    [level 0] Title: 암호 해독, Time: 0.02 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    for(int i = code-1; i<(int)cipher.size(); i+= code)
+    {
+        answer += cipher[i];
+    }
+    return answer;
+}
