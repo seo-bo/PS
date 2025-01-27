436e40b53ac13105ac14c8e3927b843406872d88
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-27 09:27:46 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = -1;
+    for(int i = 0; i<(int)num_list.size();++i)
+    {
+        if(num_list[i] < 0)
+        {
+            return i;
+        }
+    }
+    return answer;
+}
