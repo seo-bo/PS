13b5769d7b7fabdc74810cc69400c4f5bba07681
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-01-27 17:50:44 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int n = number.size();
+    for(int i =0; i<n;++i)
+    {
+        for(int j =i+1; j<n;++j)
+        {
+            for(int k = j+1;k<n;++k)
+            {
+                if(number[i] + number[j] + number[k] == 0)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
